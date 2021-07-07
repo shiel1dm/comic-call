6e0343c23025bf85aefa0ce3c6fc642f0aa25e57
shiel1dm <shield55@msu.edu>
2021-07-06 21:50:54 -0400

    fixed 401

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 //Variables
 srcBtn = $('.button')
-publicKey = '14d2733483246197c41202eeee06f856'
-privateKey = '552ae74ba5b503a685324ca9f89914a51bbb943b'
-ts = new Date().getTime();
-md5= ts+privateKey+publicKey
+publicKey = '77eadb0de3c81dae60d3ca584d03a73c'
+privateKey = '418d55284383ba9c9da775e688ebbd6c4faeffdd'
+ts = '1'
+
+md5= '0f81566c5546a9103f75756ab08b8f92'
 
 
 //Functions
 function MarApi(event){
     event.preventDefault()
-    var comicUrl = 'http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${privateKey}&hash=${md5}'
+    var comicUrl = `http://gateway.marvel.com/v1/public/characters?limit=100&ts=${ts}&apikey=${publicKey}&hash=${md5}`
     console.log('btn click works')
     fetch(comicUrl,{
         method: 'GET',
         credentials:'same-origin',
     })
         .then(function(response){
-          console.log(response)
           return response.json();
         })
         .then(function(data){
           console.log(data)
-        }
-        )
+        })
+    
 }
 
 
 srcBtn.on('click', MarApi)
+
+
+// maybe set a random into from the src results. fill the carosel. 
