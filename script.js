e38d0defa7a78b5880643a11080ea22db71662a1
jeffkorby <82245490+jeffkorby@users.noreply.github.com>
2021-07-11 19:10:48 -0400

    Added function to push results to web page.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ ts = '1'
 
 
 md5= '0f81566c5546a9103f75756ab08b8f92'
+const container = document.querySelector("#marvelImg")
 
 
 //Functions
@@ -14,38 +15,51 @@ function getrandInt(length){
   return Math.floor(Math.random() * length)
 }
 
-function MarApi(event){
-    event.preventDefault()
-    
-    var comicUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${userInput.val()}&limit=100&ts=${ts}&apikey=${publicKey}&hash=${md5}`
-    //We decided to go w/ StartsWith so a user might still find information, even though the character was misspelled
-    console.log('btn click works')
-    fetch(comicUrl,{
-        method: 'GET',
-        credentials:'same-origin',
-    })
-        .then(function(response){
-          return response.json();
+      function MarApi(event){
+        event.preventDefault()
+        
+        var comicUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${userInput.val()}&limit=100&ts=${ts}&apikey=${publicKey}&hash=${md5}`
+        //We decided to go w/ StartsWith so a user might still find information, even though the character was misspelled
+        console.log('btn click works')
+        fetch(comicUrl,{
+            method: 'GET',
+            credentials:'same-origin',
         })
-        .then(function(data){
-          i = getrandInt(data.data.results.length)
-          character= data.data.results
-          hero= character[i].name
-          url = character[i].thumbnail.path
-          ext = character[i].thumbnail.extension
-          image= url+$.trim('.'+ext)
-          
-          console.log(getrandInt(character.length)) //generates random int, but logging for test purposes.
-          console.log(character[i]) //grabs random character from array
-          console.log(hero)
-          
+            .then(function(response){
+              return response.json();
+            })
+            .then(function(data){
+              i = getrandInt(data.data.results.length)
+              character= data.data.results
+              hero= character[i].name
+              url = character[i].thumbnail.path
+              ext = character[i].thumbnail.extension
+              image= url+$.trim('.'+ext)
+              description= character[i].description
+              
+              console.log(getrandInt(character.length)) //generates random int, but logging for test purposes.
+              console.log(character[i]) //grabs random character from array
+              console.log(hero)  // logs the character name that was chosen
+              console.log(image) //logs the final combined image URL
 
-        })
+
+              let output = "";
+              output = `
+              <div>
+                <h2 class= "bold"><strong>${hero}</strong></h2>
+                <p class= "desc">${description}<p>
+                <img class= "marImg" src=${image} alt="image" />
+               </div> 
+              `
+              container.innerHTML += output
+            })
+
+
     
 }
-
-
+       
 srcBtn.on('click', MarApi)
+
 
 
 // maybe set a random into from the src results. fill the carosel. 
