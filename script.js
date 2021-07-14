a29698eab99275cd8d1c981eef5f1def2e051bb8
shiel1dm <shield55@msu.edu>
2021-07-14 14:39:52 -0400

    Changed text color so it wasn't lost in our darker background

--- script.js
+++ script.js
@@ -15,48 +15,48 @@ function getrandInt(length){
   return Math.floor(Math.random() * length)
 }
 
-      function MarApi(event){
-        event.preventDefault()
+  function MarApi(event){
+    event.preventDefault()
+    
+    var comicUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${userInput.val()}&limit=100&ts=${ts}&apikey=${publicKey}&hash=${md5}`
+    //We decided to go w/ StartsWith so a user might still find information, even though the character was misspelled
+    console.log('btn click works')
+    fetch(comicUrl,{
+      method: 'GET',
+      credentials:'same-origin',
+    })
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(data){
+        i = getrandInt(data.data.results.length)
+        character= data.data.results
+        hero= character[i].name
+        url = character[i].thumbnail.path
+        ext = character[i].thumbnail.extension
+        image= url+$.trim('/portrait_uncanny.'+ext)
+        description= character[i].description
+        urls= character[i].urls
+
         
-        var comicUrl = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${userInput.val()}&limit=100&ts=${ts}&apikey=${publicKey}&hash=${md5}`
-        //We decided to go w/ StartsWith so a user might still find information, even though the character was misspelled
-        console.log('btn click works')
-        fetch(comicUrl,{
-            method: 'GET',
-            credentials:'same-origin',
+        console.log(getrandInt(character.length)) //generates random int, but logging for test purposes.
+        console.log(character[i]) //grabs random character from array
+        console.log(hero)  // logs the character name that was chosen
+        console.log(image) //logs the final combined image URL
+        console.log(urls)
+
+
+
+        let output = "";
+        output = `
+        <div>
+          <h2 class= "bold has-text-light">${hero}</h2>
+          <img class= "marImg" src=${image} alt="image" />
+          <p class= "desc Body  has-text-light">${description}</p>
+          </div> 
+        `
+        container.innerHTML += output
         })
-            .then(function(response){
-              return response.json();
-            })
-            .then(function(data){
-              i = getrandInt(data.data.results.length)
-              character= data.data.results
-              hero= character[i].name
-              url = character[i].thumbnail.path
-              ext = character[i].thumbnail.extension
-              image= url+$.trim('/portrait_uncanny.'+ext)
-              description= character[i].description
-              urls= character[i].urls
-      
-              
-              console.log(getrandInt(character.length)) //generates random int, but logging for test purposes.
-              console.log(character[i]) //grabs random character from array
-              console.log(hero)  // logs the character name that was chosen
-              console.log(image) //logs the final combined image URL
-              console.log(urls)
-
-
-
-              let output = "";
-              output = `
-              <div>
-                <h2 class= "bold"><strong>${hero}</strong></h2>
-                <img class= "marImg" src=${image} alt="image" />
-                <p class= "desc Body">${description}</p>
-               </div> 
-              `
-              container.innerHTML += output
-            })
 
 
     
